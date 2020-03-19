e43d7755b557164208910a3e7c1711b6be78ddd5
Mark Prodehl <markprodehl@gmail.com>
2020-03-18 20:12:14 -0700

    added date to planner

---
+++ script.js
@@ -0,0 +1,9 @@
+//display time and date at the top of page
+//compare to the variable moment.js
+//convert current moment.js to hr block
+//time block is green if its future
+//time black is red if its current
+//time block is grey if its past
+// when user clicks on save button then saves to local storage
+// when they refresh page then todo list is still saved
+//(get from local storage)
