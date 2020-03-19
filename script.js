55524bfb162d1339f8de2b2e883559ab13289aeb
Mark Prodehl <markprodehl@gmail.com>
2020-03-19 00:05:31 -0700

    added date and some time elements

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 //display time and date at the top of page
-//compare to the variable moment.js
-//convert current moment.js to hr block
+var m = moment();
+// console.log(m.format("dddd, MMMM do YYYY "));
+var currentDay = document.getElementById("currentDay")
+currentDay.append(m.format("dddd, MMMM do YYYY, h:mm:ss a"));
+
+var m1 = moment() +
+    console.log(m.diff(m))
+
+// set time block with current time to red
+
+var currentHour0 = moment();
+
+currentHour0 = document.getElementById("currentHour");
+// currentHour = (m.format("h:a"));
+// currentHour.append(m.format("dddd, MMMM do YYYY, h:mm:ss a"));
+$("#currentHour0").text(currentHour0)
+
+console.log(currentHour0)
+
+var hourOne = currentHour0 + moment("1:00", 'HH')
+console.log(hourOne)
+
+//set time block with past time to grey
+
+//set time block with future times to gre
+
+// 
 //time block is green if its future
 //time black is red if its current
 //time block is grey if its past
