5d486f7ff9e13b4fb062620660397f86656f46b9
Mark Prodehl <markprodehl@gmail.com>
2020-03-19 17:26:32 -0700

    set time and aassigned to blocks

--- code I can use.js
+++ code I can use.js
@@ -7,60 +7,32 @@ var hoursData = {
     '14': '',
     '15': '',
     '16': '',
-    '17': '',
-    '18': '',
-    '19': '',
-    '20': ''
+    '17': ''
 };
-
-var m = moment();
-// console.log(m.format("dddd, MMMM do YYYY "));
-//let currentDay be the current date on the screen
-var currentDay = document.getElementById("currentDay")
-currentDay.append(m.format("dddd, MMMM do YYYY"));
-
-// let currentHour be the hour of the current time of day
-var currentHour = moment()
-currentHour = parseInt(moment().format("H"));
-// console.log(currentHour)
-
+var currentHour = parseInt(moment().format("H"));
 // check localStorage for hours data
-
-
 // update hoursData if localStorage for hours data isn't empty
 // how to determine if time is future/present/past
 // let currentHour be the hour of the current time of day
-
-
 // get reference to all the hours
 var timeBlockElements = $(".time-block")
-    // console.log(timeBlockElements)
     // for each hour
 timeBlockElements.each(function() {
     // get the hour of the block
     // console.log(this);
     var timeBlockHour = parseInt(this.id.split("-")[1]);
-    // console.log(this)
     // get description from hoursData
     var description = hoursData[timeBlockHour.toString()];
     // display the description
-    // console.log(description)
     $(this)
         .find("textarea")
         .val(description);
-    console.log(timeBlockHour, currentHour);
-
+    // console.log(timeBlockHour, currentHour);
     if (timeBlockHour < currentHour) {
         $(this).addClass("past")
-        console.log(this)
     } else if (timeBlockHour === currentHour) {
         //   give present class to the hour
-        $(this).addClass("present")
     } else {
         //   give future class to the hour
-        $(this).addclass("future")
     }
-
 });
-
-//make styles for past, present and future see css
